5c1ea2e6ab19edd0cacc096d4281999f161fb5f9
jaytwitch <johnathan.tiong@gmail.com>
2021-01-28 12:12:26 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MPU Maid Discord Bot
+
+This is a discord bot written in Python! It's designed to help make things fun and easy for the Multiplayer United Community!
+DO NOT USE WITHOUT PERMISSION
+
+Install the Discord.py, and Python-GValve libraries -
+
+```
+python3 -m pip install -U discord.py
+python3 -m pip install -U python-valve
+```
+
+Then, run:
+
+```
+python3 maid.py
+```
---
+++ maid.py
@@ -0,0 +1,75 @@
+# maid.py
+#
+# MPU Maid Bot
+#
+# Creator: Johnathan Tiong <johnathan.tiong@gmail.com>
+#
+import os
+import secrets
+import mysql.connector
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+secretsGenerator = secrets.SystemRandom()
+
+try:
+    mydb = mysql.connector.connect(
+        host = os.getenv('DB_HOST'),
+        user = os.getenv('DB_USER'),
+        passwd = os.getenv('DB_PASS'),
+        database = os.getenv('DB_NAME'),
+    )
+
+    if mydb.is_connected():
+        cursor = mydb.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Connection to DB established: ", record)
+        print('-----------------------------------------')
+
+        maidCreateLogTableSQL = """
+        CREATE TABLE IF NOT EXISTS maid_log (
+            id INT AUTO_INCREMENT,
+            user VARCHAR(200),
+            action TEXT,
+            time INT,
+            PRIMARY KEY (id))"""
+
+        cursor.execute(maidCreateLogTableSQL)
+
+except Error as e:
+    print("Error connecting to DB: ", e)
+
+@bot.command(name='hello', help='Responds to a hello message from a user')
+async def hello(ctx):
+    msg = "Hello there, {0.author.mention}!".format(ctx.message)
+    await ctx.send(msg)
+
+@bot.command(name='roll', help='roll [number of dice] [number of sides per die]')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(secretsGenerator.randrange(1, number_of_sides + 1))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@bot.command(name='site', help='Retrieves specific website details')
+
+@bot.event
+async def on_ready():
+    print('Logged in as ')
+    print(bot.user.name)
+    print('-------------')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandInvokeError):
+        await ctx.send(":x: Can't send response correctly (message too long)")
+
+bot.run(token)
+
